1a3e5b77edbe8059fd90fb80251880c9e7652a8b
Chance Strickland <hi@chance.dev>
2023-01-14 20:36:45 -0800

    use-event-listener API changes

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -24,10 +24,27 @@
 
 - Simplified build tooling which removed some polyfills for older browsers
 - React v18+ is now a peer dependency
-- `useUpdateEffect` has been removed, as it will not work with React v18's concurrent features or in `<React.StrictMode>`
-- `useEventListener` no longer saves the handler in a ref. Consumers should be expected to provide their own memoized callback to prevent unnecessarily adding and removing the listener in every render cycle.
+- `useUpdateEffect` has been removed, as it will not work with React v18's
+  concurrent features or in `<React.StrictMode>`
+- `useEventListener` has changed in several respects:
+
+  - The hook signature now has two overloads. If the first argument is a ref,
+    its value will be used as the element to which the listener is attached.
+    Otherwise, the first argument is the event type and the listener is attached
+    to the `window` object.
+    ```ts
+    function useEventListener(type, listener, options) {}
+    function useEventListener(elementRef, type, listener, options) {}
+    ```
+  - The listener function is no longer saved in a ref internally, which means it
+    will be attached/removed on every render unless you memoize it with
+    `useCallback`
+  - `useLayoutEventListener` has been renamed to `useEventListenerLayoutEffect`
+
 - `useStableCallback` now updates the callback in `useLayoutEffect` instead of `useEffect` by default
-- The `history` array returned from `useStateWithHistory` is now stateful instead of a ref. This is more aligned with the React mental model when provided as a dependency, and it prevents issues with stale closures.
+- The `history` array returned from `useStateWithHistory` is now stateful
+  instead of a ref. This is more aligned with the React mental model when
+  provided as a dependency, and it prevents issues with stale closures.
 - Removed `use-media` module. `useMatchMedia` should be imported from `use-match-media` instead
 - All default exports have been removed in favor of named exports
 
--- src/use-event-listener.ts
+++ src/use-event-listener.ts
@@ -2,16 +2,86 @@ import * as React from "react";
 import { useLayoutEffect } from "./use-isomorphic-layout-effect";
 
 function createUseEventListener(useEffect: typeof React.useEffect) {
-	return function <
+	/**
+	 * A React hook that attaches an event listener to the global window. The
+	 * listener is attached in an effect hook and torn down in its cleanup phase.
+	 *
+	 * @param type A case-sensitive string representing the [event
+	 * type](https://developer.mozilla.org/en-US/docs/Web/Events) to listen for
+	 * @param listener The event listener callback that fires in response to the
+	 *                 event being dispatched
+	 * @param options An options object that specifies characteristics about the
+	 *                event listener
+	 */
+	function useEventListener<ListenerType extends keyof EventMap>(
+		type: ListenerType,
+		listener: (event: EventMap[ListenerType]) => void,
+		options?: CreateUseEventListenerOptions
+	): void;
+
+	/**
+	 * A React hook that attaches an event listener to an element stored in a ref.
+	 * The listener is attached in an effect hook and torn down in its cleanup
+	 * phase.
+	 *
+	 * @param elementRef A React ref object containing the element to which the
+	 *                   event listener will be attached
+	 * @param type A case-sensitive string representing the [event
+	 * type](https://developer.mozilla.org/en-US/docs/Web/Events) to listen for
+	 * @param listener The event listener callback that fires in response to the
+	 *                 event being dispatched
+	 * @param options An options object that specifies characteristics about the
+	 *                event listener
+	 */
+	function useEventListener<
 		ElementType extends (Window & typeof globalThis) | Document | Element,
 		ListenerType extends keyof EventMap
 	>(
-		eventName: ListenerType,
-		callback: (event: EventMap[ListenerType]) => void,
-		// @ts-ignore
-		element: ElementType = global,
-		options?: AddEventListenerOptions
+		elementRef: React.RefObject<ElementType>,
+		type: ListenerType,
+		listener: (event: EventMap[ListenerType]) => void,
+		options?: CreateUseEventListenerOptions
+	): void;
+
+	function useEventListener(
+		elementOrType: unknown,
+		typeOrListener: unknown,
+		listenerOrOptions?: unknown,
+		options?: CreateUseEventListenerOptions
 	) {
+		let element: (Window & typeof globalThis) | Document | Element | undefined;
+		let type: keyof EventMap;
+		let listener: (event: EventMap[typeof type]) => void;
+		let elementRef: React.RefObject<typeof element> | null = null;
+		if (typeof elementOrType === "string") {
+			type = elementOrType as keyof EventMap;
+			if (typeof typeOrListener !== "function") {
+				throw new Error(
+					`Expected event handler callback to be a function; received ${typeof typeOrListener}`
+				);
+			}
+			listener = typeOrListener as typeof listener;
+			options = (listenerOrOptions as CreateUseEventListenerOptions) || {};
+		} else if (
+			elementOrType &&
+			typeof elementOrType === "object" &&
+			"current" in elementOrType
+		) {
+			elementRef = elementOrType as React.RefObject<typeof element>;
+			type = typeOrListener as keyof EventMap;
+			if (typeof listenerOrOptions !== "function") {
+				throw new Error(
+					`Expected event listener to be a function; received ${typeof listenerOrOptions}`
+				);
+			}
+			listener = listenerOrOptions as typeof listener;
+			options = options || {};
+		} else {
+			throw new Error(
+				`Expected first argument to be an event type string or element ref; received ${typeof elementOrType}`
+			);
+		}
+
 		let {
 			capture,
 			once,
@@ -21,25 +91,54 @@ function createUseEventListener(useEffect: typeof React.useEffect) {
 			signal,
 		} = options || {};
 		useEffect(() => {
+			let element = elementRef ? elementRef.current : window;
 			let opts = { capture, passive, once, signal };
-			element.addEventListener(eventName, listener, opts);
+			element?.addEventListener(type, listener, opts);
 			return () => {
-				element.removeEventListener(eventName, listener, opts);
+				element?.removeEventListener(type, listener, opts);
 			};
-			function listener(event: any) {
-				callback(event);
-			}
-		}, [element, eventName, callback, capture, passive, once, signal]);
-	};
+		}, [listener, capture, elementRef, once, passive, signal, type]);
+	}
+	return useEventListener;
 }
 
 export const useEventListener = createUseEventListener(React.useEffect);
+
+/**
+ * A React hook that adds an event listener to a given element. If no element is
+ * specified, the listener is attached to the global object.
+ *
+ * The event listener is added and removed within a `useLayoutEffect`.
+ */
 export const useEventListenerLayoutEffect =
 	createUseEventListener(useLayoutEffect);
 
-/**
- * @alias useEventListenerLayoutEffect
- * @deprecated
- */
-export const useLayoutEventListener = useEventListenerLayoutEffect;
 export type EventMap = ElementEventMap & DocumentEventMap & WindowEventMap;
+
+export interface CreateUseEventListenerOptions {
+	/**
+	 * A boolean value indicating that events of this type will be dispatched to
+	 * the registered `listener` before being dispatched to any `EventTarget`
+	 * beneath it in the DOM tree. If not specified, defaults to `false`.
+	 */
+	capture?: boolean;
+	/**
+	 * A boolean value indicating that the `listener` should be invoked at most
+	 * once after being added. If `true`, the `listener` would be automatically
+	 * removed when invoked. If not specified, defaults to `false`.
+	 */
+	once?: boolean;
+	/**
+	 * A boolean value that, if `true`, indicates that the function specified by
+	 * `listener` will never call `preventDefault()`. If a passive listener does
+	 * call `preventDefault()`, the user agent will do nothing other than generate
+	 * a console warning. If not specified, defaults to `false`.
+	 */
+	passive?: boolean;
+	/**
+	 * An `AbortSignal`. The `listener` will be removed when the given `AbortSignal`
+	 * object's `abort()` method is called. If not specified, no `AbortSignal` is
+	 * associated with the `listener`.
+	 */
+	signal?: AbortSignal;
+}
