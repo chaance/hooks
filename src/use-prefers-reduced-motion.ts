ee311c2f9e1ea6e12931ece575bd7a20dade113e
Chance Strickland <hi@chance.dev>
2025-05-25 12:38:01 -0700

    `usePrefersReducedMotion`: ensure state is set on initial match media check

--- src/use-prefers-reduced-motion.ts
+++ src/use-prefers-reduced-motion.ts
@@ -4,33 +4,17 @@ const PREFERS_REDUCED_MOTION_NO_PREF_QUERY =
 	"(prefers-reduced-motion: no-preference)";
 
 export function usePrefersReducedMotion(nodeRef: React.RefObject<Element>) {
-	let [prefersReducedMotion, setPrefersReducedMotion] = useState(false);
+	let [state, setState] = useState(false);
 	useEffect(() => {
-		return onPrefersReducedMotionChange(
-			setPrefersReducedMotion,
-			nodeRef.current?.ownerDocument?.defaultView || window,
-		);
+		const globalWindow = nodeRef.current?.ownerDocument?.defaultView || window;
+		let mql = globalWindow.matchMedia(PREFERS_REDUCED_MOTION_NO_PREF_QUERY);
+		let controller = new AbortController();
+		mql.addEventListener("change", (event) => setState(!event.matches), {
+			signal: controller.signal,
+		});
+		setState(!mql.matches);
+		return () => controller.abort();
 	}, [nodeRef]);
-	return prefersReducedMotion;
-}
 
-function onPrefersReducedMotionChange(
-	callback: (prefers: boolean) => any,
-	globalWindow: Window & typeof globalThis,
-) {
-	try {
-		let mediaQueryList = globalWindow.matchMedia(
-			PREFERS_REDUCED_MOTION_NO_PREF_QUERY,
-		);
-		mediaQueryList.addEventListener("change", listener);
-		return () => {
-			mediaQueryList.removeEventListener("change", listener);
-		};
-	} catch (e) {
-		return () => {};
-	}
-
-	function listener(event: MediaQueryListEvent) {
-		callback(!event.matches);
-	}
+	return state;
 }
